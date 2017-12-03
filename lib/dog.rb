837c71bf3f3578ae33b1f1096348436c8be51400
Robert Watson <spartan83@gmail.com>
2017-12-03 21:45:12 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,9 +1,28 @@
 # Your code goes here!
 class Dog
 def initialize(name="Fido",bark="Woof!")
-@name=name
-@bark=bark
+  @name=name
+  @bark=bark
 end
+def name=(new_name)
+  @name=newname
+end
+def name
+  @name
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 end
